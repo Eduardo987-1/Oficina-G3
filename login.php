1374c491354596bc1eb751929d4a1ffe3ba7931f
Carlos Jonas <jonas.fonseca@opovodigital.com>
2021-11-18 22:10:36 -0300

    começando a página de login

---
+++ login.php
@@ -0,0 +1,14 @@
+<?php include_once 'header.php' ?>
+	
+	
+	<h1>Página de login</h1>
+
+	<form action="" method="POST">
+		Email: <input type="email" name="email"> <br>
+		Senha: <input type="password" name="senha">
+	</form>
+
+	<p>Você precisa estar cadastrado para fazer login caso você não seja cadastrado clique <a href="cadastro.php">Aqui</a></p>
+
+
+<?php include_once 'footer.php' ?>

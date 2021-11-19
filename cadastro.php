6def8990b5ce0694f319ff9b573abe3ca1ff5fe4
Carlos Jonas <jonas.fonseca@opovodigital.com>
2021-11-18 22:09:23 -0300

    começando a página de cadastro

---
+++ cadastro.php
@@ -0,0 +1,10 @@
+<?php include_once 'header.php' ?>
+    
+    <h1>Página de cadastro</h1>
+    <form>
+        Nome: <input type="text" name="nome">
+        Email: <input type="email" name="email">
+        Senha: <input type="text" name="senha">
+    </form>
+
+<?php include_once 'footer.php' ?>

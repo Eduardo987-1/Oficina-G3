002891e8dff6dff28928eafd2b4d84e83d7119fe
Carlos Jonas <jonas.fonseca@opovodigital.com>
2021-11-18 22:12:42 -0300

    linkando algumas partes do menu

--- index.php
+++ index.php
@@ -1,60 +1,147 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Oficina G3</title>
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
-</head>
-<body>
-	<!--Menu-->
-	<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled">Disabled</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-
-<!--Slide carro e moto-->
-<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="..." class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="...">
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
-</body>
-</html>
+<?php include_once 'header.php' ?>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark  fixed-top">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">CentroCar</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarText">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#servicos">Serviços</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#comentarios">Comentários</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#endereco">Endereço</a>
+              </li>
+            </ul>
+            <span class="navbar-text">
+              <a class="nav-link" href="login.php">Login</a>
+              
+            </span>
+          </div>
+        </div>
+      </nav>
+   
+        <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            </div>
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img src="https://www.mte-thomson.com.br/wp-content/uploads/2021/07/Manutencao_Preventiva-2.jpg" class="d-block w-100" style="object-fit: cover;" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                  <h5>First slide label</h5>
+                  <p>Some representative placeholder content for the first slide.</p>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <img src="https://www.industriahoje.com.br/wp-content/uploads/2016/06/manuten%C3%A7%C3%A3o-de-moto.png" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                  <h5>Second slide label</h5>
+                  <p>Some representative placeholder content for the second slide.</p>
+                </div>
+              </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Next</span>
+            </button>
+          </div>
+    <section>
+      <div class="container" id="servicos">
+        <div class="row">
+          <div class="col-12 text-center"><h1><Strong>Serviços</Strong></h1>
+            <br>
+          </div>
+            <div class="col-12 col-md-6 col-lg-3">
+                <div class="card">
+                    <img src="https://cdn.autopapo.com.br/box/uploads/2018/12/06144839/troca-de-oleo-motor-carro-shutterstock.jpg" class="card-img-top img-fluid" alt="...">
+                    
+                    <div class="card-body text-center">
+                        <h5 class="card-title">Troca de Óleo</h5>
+                        <p class="card-text">Batman originated from an incident in Bruce's childhood; after witnessing the murder of his parents.</p>
+                        
+                        <a href="#" class="btn btn-primary">Clique aqui</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-3">
+              <div class="card">
+                  <img src="https://quatrorodas.abril.com.br/wp-content/uploads/2018/12/MKIV8963cc.psd_.jpg?quality=70&strip=i" class="card-img-top" alt="...">
+                  
+                  <div class="card-body text-center">
+                      <h5 class="card-title">Alinhamento</h5>
+                      <p class="card-text">Batman originated from an incident in Bruce's childhood; after witnessing the murder of his parents.</p>
+                      
+                      <a href="#" class="btn btn-primary">Clique aqui</a>
+                  </div>
+              </div>
+          </div>
+          <div class="col-12 col-md-6 col-lg-3">
+            <div class="card">
+                <img src="https://blog.nakata.com.br/wp-content/uploads/2018/11/251606-tarefa-para-3010-ate-17h-como-identificar-um-servico-mecanico-de-qualidade-768x513.jpg" class="card-img-top" alt="...">
+                
+                <div class="card-body text-center">
+                    <h5 class="card-title">Serviços Gerais</h5>
+                    <p class="card-text">Batman originated from an incident in Bruce's childhood; after witnessing the murder of his parents.</p>
+                    
+                    <a href="#" class="btn btn-primary">Clique aqui</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-md-6 col-lg-3">
+          <div class="card">
+              <img src="https://cdn.shopify.com/s/files/1/2617/8854/articles/funilaria_e_pintura_3500x.jpg?v=1586978323" class="card-img-top" alt="...">
+              
+              <div class="card-body text-center">
+                  <h5 class="card-title">Pintura Funilaria</h5>
+                  <p class="card-text">Batman originated from an incident in Bruce's childhood; after witnessing the murder of his parents.</p>
+                  
+                  <a href="#" class="btn btn-primary">Clique aqui</a>
+              </div>
+          </div>
+      </div>
+        </div>
+      </div>
+      
+     </section>
+     <br>
+     <section style="height: 50vh;">
+       <!--COMENTARIOS AQUI-->
+       <div class="container" id="comentarios">
+        <div class="row">
+          <h1 class="text-center"><strong>Comentários</strong></h1>
+        
+        </div>
+        </div>
+       </section>
+       <section style="height: 5vh;">
+         <!--Endereço-->
+         <div class="container" id="endereco">
+          <div class="row">
+            <h1 class="text-center"><strong>Endereço</strong></h1>
+            <div class="col">
+              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3981.1990545136678!2d-38.57710688592992!3d-3.7668161444221475!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7c74c01797285bb%3A0xa8888be05f940563!2sCentro%20Universit%C3%A1rio%20Est%C3%A1cio%20-%20Campus%20Parangaba!5e0!3m2!1spt-BR!2sbr!4v1636395402645!5m2!1spt-BR!2sbr" 
+              width="550" height="400" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+            </div>
+            <div class="col">
+              <img src="https://img.icons8.com/wired/40/000000/map.png"/>Centro Universitário Estácio - Campus Parangaba
+              <img src="https://img.icons8.com/ios/32/000000/phone.png"/>0800 880 6767
+              <img src="https://img.icons8.com/dotty/49/000000/mail-contact.png"/> teste@estacio.com.br
+            </div>
+         </div>
+          </div>
+       </section>
+    
+<?php include_once 'footer.php' ?>
